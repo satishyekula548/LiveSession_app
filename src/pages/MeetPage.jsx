21515584ad11687af6e343689da9deae99dd4da3
Aditya Bhate <adityabhate305@gmail.com>
2023-04-28 22:58:05 +0530

    first commit

---
+++ src/pages/MeetPage.jsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useCallback, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { MeetContext } from "../context/MeetContext";
+
+const MeetPage = ({ match }) => {
+	//AS OF NOW DOMAIN WOULD BE JITSI'S AS WE ARE STILL USING THIER SERVERS
+	const domain = "172-232-77-224.ip.linodeusercontent.com";
+	let api = {};
+
+	const history = useHistory();
+
+	// THIS IS TO EXTRACT THE NAME WHICH WAS FILLED IN THE FIRST PAGE
+	const [name] = useContext(MeetContext);
+
+	// INTIALISE THE MEET WITH THIS FUNCTION
+	const startMeet = useCallback(() => {
+		const options = {
+			roomName: match.params.id,
+			width: "100%",
+			height: 500,
+			configOverwrite: { prejoinPageEnabled: false },
+			interfaceConfigOverwrite: {
+				// overwrite interface properties if you want
+			},
+			// VIDEO FRAME WILL BE ADDED HERE
+			parentNode: document.querySelector("#jitsi-iframe"),
+			userInfo: {
+				displayName: name,
+			},
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		api = new window.JitsiMeetExternalAPI(domain, options);
+
+		api.addEventListeners({
+			readyToClose: handleClose,
+			participantLeft: handleParticipantLeft,
+			participantJoined: handleParticipantJoined,
+			videoConferenceJoined: handleVideoConferenceJoined,
+			videoConferenceLeft: handleVideoConferenceLeft,
+		});
+	}, [api]);
+
+	useEffect(() => {
+		if (window.JitsiMeetExternalAPI) {
+			startMeet();
+		} else {
+			alert("JitsiMeetExternalAPI not loaded");
+		}
+	}, [startMeet]);
+
+	// ALL OUR HANDLERS
+	const handleClose = () => {
+		console.log("handleClose");
+	};
+
+	const handleParticipantLeft = async (participant) => {
+		console.log("handleParticipantLeft", participant);
+		await getParticipants();
+	};
+
+	const handleParticipantJoined = async (participant) => {
+		console.log("handleParticipantJoined", participant);
+		await getParticipants();
+	};
+
+	const handleVideoConferenceJoined = async (participant) => {
+		console.log("handleVideoConferenceJoined", participant);
+		await getParticipants();
+	};
+
+	const handleVideoConferenceLeft = () => {
+		console.log("handleVideoConferenceLeft");
+		history.push("/thank-you");
+	};
+
+	// GETTING ALL PARTICIPANTS
+	const getParticipants = () => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve(api.getParticipantsInfo());
+			}, 500);
+		});
+	};
+
+	return (
+		<React.Fragment>
+			<header
+				style={{
+					backgroundColor: "rgb(10, 25, 41)",
+					color: "white",
+					textAlign: "center",
+				}}>
+				<p style={{ margin: 0, padding: 10 }}>Session</p>
+			</header>
+			<div
+				id='jitsi-iframe'
+				style={{
+					width: "100%",
+					height: "100%",
+					marginBottom: 0,
+					marginTop: "10px",
+				}}></div>
+			<div
+				style={{
+					backgroundColor: "rgb(10, 25, 41)",
+					height: "10vh",
+					margin: 0,
+				}}></div>
+		</React.Fragment>
+	);
+};
+
+export default MeetPage;
---
+++ src/pages/StartupPage.jsx
@@ -0,0 +1,125 @@
+import React, { useContext, useState } from "react";
+import {
+	Paper,
+	makeStyles,
+	TextField,
+	Button,
+	Snackbar,
+} from "@material-ui/core";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import { MeetContext } from "../context/MeetContext";
+import MuiAlert from "@material-ui/lab/Alert";
+import { generateString } from "../helper/generateRandomString";
+
+// Alert when the user hasn't filled up their name
+function Alert(props) {
+	return <MuiAlert elevation={6} variant='filled' {...props} />;
+}
+
+const AlertBar = ({ open, handleClose }) => (
+	<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+		<Alert onClose={handleClose} severity='error'>
+			Enter your name please!
+		</Alert>
+	</Snackbar>
+);
+
+// stylings for the page
+const useStyles = makeStyles(() => ({
+	background: {
+		backgroundColor: "rgb(10, 25, 41)",
+		height: "100vh",
+		width: "100%",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		colorScheme: "dark",
+		flexDirection: "column",
+	},
+	card: {
+		backgroundColor: "rgb(0, 30, 60)",
+		colorScheme: "dark",
+		border: "1px solid rgb(19, 47, 76)",
+		color: "white",
+		padding: "3rem",
+	},
+	input: {
+		width: "350px",
+	},
+}));
+
+const StartupPage = () => {
+	const classes = useStyles();
+
+	// we will be preferring dark theme for our page
+	const theme = React.useMemo(
+		() =>
+			createTheme({
+				palette: {
+					type: "dark",
+				},
+			}),
+		[]
+	);
+
+	// we will use this to navigate next page
+	const history = useHistory();
+
+	// will be using name across all pages from context
+	const [name, setName] = useContext(MeetContext);
+
+	// state and handler function for the snackbar
+	const [open, setOpen] = useState(false);
+
+	const handleClick = () => {
+		setOpen(true);
+	};
+
+	const handleClose = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
+		setOpen(false);
+	};
+
+	return (
+		<div className={classes.background}>
+			<Paper className={classes.card} elevation={4}>
+				<h4>NeatSkills Live Session</h4>
+				<ThemeProvider theme={theme}>
+					<div style={{ marginBottom: "1.5rem" }}>
+						<TextField
+							label='Name'
+							variant='outlined'
+							color='default'
+							className={classes.input}
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</div>
+					<div style={{ marginBottom: "1.5rem", textAlign: "center" }}>
+						<Button
+							variant='contained'
+							color='default'
+							onClick={() => {
+								// if name is empty we mandate user to enter it as we also trigger to open snackbar here
+								if (name === "") {
+									handleClick();
+									return;
+								}
+
+								// if all goes well we will be redirecting the user to meet room
+								history.push(`/meet/${generateString(7)}`);
+							}}>
+							Create Meet
+						</Button>
+					</div>
+				</ThemeProvider>
+			</Paper>
+			<AlertBar open={open} handleClose={handleClose} />
+		</div>
+	);
+};
+
+export default StartupPage;
